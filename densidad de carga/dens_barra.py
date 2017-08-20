c92bed32acaf140fe924ae7f1de90ff7a99a5382
vicmans <vsanchezl94@gmail.com>
2017-08-20 14:51:03 -0400

    Primeros archivos
    Calculo de densidad de carga de una barra y de un plano

---
+++ densidad de carga/dens_barra.py
@@ -0,0 +1,40 @@
+import numpy as np
+import math
+
+a=float(input('radio: '))
+l=float(input('longitud: '))
+N=input('# de elementos: ')
+v=input('voltaje: ')
+
+delta=float(l/float(N))
+print delta
+
+iguales=2*math.log((delta/2 + math.sqrt(a**2 + (delta/2)**2))/a)
+print "iguales", iguales
+Z=np.zeros((N,N))
+for m in range(1,N+1):
+    for n in range(1,N+1):
+        if m==n:
+            Z[m-1,n-1]=iguales;
+        elif math.fabs(m-n)<=2:
+           dmas=math.fabs((n-m))*delta + delta/2
+           dmenos=math.fabs((n-m))*delta - delta/2
+           Z[m-1,n-1]=math.log((dmas + math.sqrt((dmas)**2 + a**2))/(dmenos + math.sqrt((dmenos)**2 + a**2)))
+        else:
+            dmas=math.fabs((n-m))*delta + delta/2
+            dmenos=math.fabs((n-m))*delta - delta/2
+            Z[m-1,n-1]=math.log(dmas/dmenos)
+print(Z)
+V=np.zeros((N, 1))
+for i in range(0,N):
+    V[i]=v*4*math.pi*8.854187*10**(-12);
+
+I=np.linalg.solve(Z,V);
+print(I)
+
+import matplotlib.pyplot as plt
+x = np.arange(0., l, delta)
+plt.plot(x,I)
+plt.ylabel('Densidad de carga')
+plt.xlabel('Longitud barra')
+plt.show()
---
+++ densidad de carga/plano.py
@@ -0,0 +1,71 @@
+import numpy as np
+import math
+
+a2=float(input('Longitud del largo del cuadrito pequeno: '))
+l=float(input('lado del plano: '))
+v=float(input('voltaje: '))
+#a2=0.2
+#l=1
+#v=1
+a=a2/2
+N=int(l/a2)
+N=N**2
+
+print "Son ",N,"parches (cuadraditos)"
+
+
+epsilon=8.85*10**(-12)
+iguales=(2*a/math.pi/epsilon)*np.log(1+np.sqrt(2))
+area=a2**2
+
+#Para hacer el grillado a partir de los datos
+x = np.arange(a, l, 2*a)
+y = np.arange(a, l, 2*a)
+xx, yy = np.meshgrid(x, y)
+
+#print "iguales", iguales
+Z=np.zeros((N,N))
+xf=np.append([], xx)
+yf=np.append([], yy)
+for m in range(0,N):
+    for n in range(0,N):
+      if m==n:
+        Z[m,n]=iguales;
+      else:
+        Z[m,n]=area/np.sqrt( (xf[m]-xf[n])**2 + (yf[m]-yf[n])**2 )
+print(Z)
+V=np.zeros((N, 1))
+for i in range(0,N):
+    V[i]=v*4*math.pi*8.854187*10**(-12);
+
+I=np.linalg.solve(Z,V);
+N=int(np.sqrt(N))
+I=np.reshape(I, (N, N))
+#para graficar
+from mpl_toolkits.mplot3d import Axes3D  
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+colortuple = ('y', 'b')
+colors = np.empty(xx.shape, dtype=str)
+for y in range(len(yy)):
+    for x in range(len(xx)):
+        colors[x, y] = colortuple[(x + y) % len(colortuple)]
+
+# Plot the surface with face colors taken from the array we made.
+## Mejorar el color:
+#my_col = cm.jet(Z/np.amax(Z))
+
+surf = ax.plot_surface(xx, yy, I, rstride=1, cstride=1, color='b',
+        linewidth=0.1, antialiased=False)
+
+plt.title(u'Densidad de carga de un plano')
+plt.xlabel('Eje x')
+plt.ylabel('Eje y')
+# Mostramos en pantalla
+plt.show()
+	
